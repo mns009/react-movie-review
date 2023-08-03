0d3463cafac039790a1522c3568ec7aaa068a03d
shoaib <shoaib903290@gmail.com>
2023-08-03 22:58:29 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
+import StarComponent from './StarComponent';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarComponent maxRating={10}/>
   </React.StrictMode>
 );
 
